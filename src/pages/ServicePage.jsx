045618c8b8c1949b150c27b826882dbe85f5bf6a
Krishna <btech10725.22@bitmesra.ac.in>
2025-06-11 13:48:06 +0530

    vercel config

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -1,7 +1,6 @@
 import ServiceCard from "../components/ServiceCard";
-import { posters, podcasts, videography } from "../utils/Constants";
+import { posters, podcasts, videography, BundlePack } from "../utils/Constants";
 import Coming from "../assets/Coming.jpg";
-
 
 const ServiceSection = ({ title, services }) => (
 	<div className="border-black w-full mb-16">
@@ -26,6 +25,32 @@ const ServiceSection = ({ title, services }) => (
 	</div>
 );
 
+// const BundlePackage = ({ bundlePack }) => (
+//     <div className="">
+//         <div className="relative w-full ">
+//             <h1 className="font-serif w-full text-center text-black text-4xl sm:text-6xl font-bold drop-shadow-lg">
+//                 BUNDLED PACKAGES
+//             </h1>
+//             <p className="font-mono text-slate-800 text-center w-full">
+//                 (Best Value)
+//             </p>
+//         </div>
+//         <div className="flex flex-col sm:flex-row flex-wrap justify-center gap-y-12">
+//             {bundlePack &&
+//                 bundlePack.map((service, index) => (
+//                     <ServiceCard
+//                         key={index}
+//                         title={service.title}
+//                         description={service.description}
+//                         features={service.features}
+//                         Icon={service.icon}
+//                         price={service.price}
+//                     />
+//                 ))}
+//         </div>
+//     </div>
+// )
+
 const Service = () => {
 	return (
 		<div className="w-full overflow-x-hidden">
@@ -33,6 +58,18 @@ const Service = () => {
 				<h1 className="text-2xl sm:text-3xl font-bold my-8 w-full text-center">
 					Our Marketing Services
 				</h1>
+				<p className="text-gray-800 mb-6 mx-3 text-lg text-wrap text-center">
+					It's Your <strong>Industry's Time</strong> to Shine, with{" "}
+					<strong>Industries Times</strong>! We don't just report success; we
+					create it. We craft magnetic posters, viral-ready reels, engaging
+					podcasts, cinematic videography, and provide razor-sharp editing to
+					make your business iconic. Get all your marketing firepower in one
+					place and dominate your sector.
+				</p>
+
+				{/* Bundled Package */}
+				{/* <BundlePackage bundlePack={BundlePack} /> */}
+
 
 				<ServiceSection title="Posters" services={posters} />
 				<ServiceSection title="Podcast Shooting" services={podcasts} />
